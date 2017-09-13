4aeee587461788567e4727f9436eff71d09e5340
Vic Lin <yichun03@gmail.com>
2017-09-13 22:47:05 +0800

    google map test

--- public/js/googlemap.js
+++ public/js/googlemap.js
@@ -1,8 +1,7 @@
 var map;
 function initMap() {
  
-
-  var center = {lat:24.438160, lng:118.421481};
+  var center = {lat:24.788068, lng:120.999012};
   map = new google.maps.Map(document.getElementById('map-canvas'), {
     center: center,
     zoom: 14
