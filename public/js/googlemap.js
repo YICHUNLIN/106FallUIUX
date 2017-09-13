ecfdc19a6091a145502ee1e740144d4159adc6c6
Vic Lin <yichun03@gmail.com>
2017-09-13 22:39:30 +0800

    google map test

--- public/js/googlemap.js
+++ public/js/googlemap.js
@@ -3,7 +3,7 @@ function initMap() {
  
 
   var center = {lat:24.438160, lng:118.421481};
-  map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
     center: center,
     zoom: 14
   });
