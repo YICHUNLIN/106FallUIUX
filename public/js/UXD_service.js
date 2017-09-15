9eb28ed133cf3d18f0f160f56d144b3a508ebe81
Vic Lin <yichun03@gmail.com>
2017-09-15 16:53:31 +0800

    ssl

--- public/js/UXD_service.js
+++ public/js/UXD_service.js
@@ -1,7 +1,7 @@
 var service = angular.module('UXD_services',[]);
 
-//var api_addr = "127.0.0.1:3000";
-var api_addr = "uiux.kmtree.me";
+var api_addr = "127.0.0.1:3000";
+//var api_addr = "uiux.kmtree.me";
 
 service.factory('MailService',['$http',function($http){
 	return {
