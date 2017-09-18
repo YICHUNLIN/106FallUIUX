02e47ae382469fc40dc333fc401d37c260d4a8b9
Vic Lin <yichun03@gmail.com>
2017-09-18 21:22:54 +0800

    option

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var db = require('./db');
+//var db = require('./db');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var mail = require('./routes/mail');
